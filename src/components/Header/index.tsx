f2a6f04b5ba394a1c0a64866377b8df92f54ddec
Gui-dev <adrianon10@gmail.com>
2022-12-07 04:21:19 -0300

    feat: add component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Text } from '../Text'
+
+import { Container } from './style'
+
+export const Header = () => {
+  return (
+    <Container>
+      <Text opacity={0.9} size={14}>Bem vindo(a) ao</Text>
+      <Text size={24} weight="700">
+        WAITER<Text size={24} weight="400">APP</Text>
+      </Text>
+    </Container>
+  )
+}
