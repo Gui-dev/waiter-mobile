23f32653b983a7a5874e453909657c67631b5c98
Gui-dev <adrianon10@gmail.com>
2022-12-10 03:14:55 -0300

    feat: add table number and helper function onCancelOrder

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,35 @@
+import { TouchableOpacity } from 'react-native'
 import { Text } from '../Text'
 
-import { Container } from './style'
+import { Container, Content, OrderHeader, Table } from './style'
 
-export const Header = () => {
+type HeaderProps = {
+  table: string
+  onCancelOrder: () => void
+}
+
+export const Header = ({ table, onCancelOrder }: HeaderProps) => {
   return (
     <Container>
-      <Text opacity={0.9} size={14}>Bem vindo(a) ao</Text>
-      <Text size={24} weight="700">
-        WAITER<Text size={24} weight="400">APP</Text>
-      </Text>
+      {!table
+        ? <>
+          <Text opacity={0.9} size={14}>Bem vindo(a) ao</Text>
+          <Text size={24} weight="700">
+            WAITER<Text size={24} weight="400">APP</Text>
+          </Text>
+        </>
+        : <Content>
+          <OrderHeader>
+            <Text size={24} weight="600">Pedido</Text>
+            <TouchableOpacity onPress={onCancelOrder}>
+              <Text color="#D73035" size={14} weight="600">cancelar pedido</Text>
+            </TouchableOpacity>
+          </OrderHeader>
+          <Table>
+            <Text color="#666">Mesa {table}</Text>
+          </Table>
+        </Content>
+      }
     </Container>
   )
 }
