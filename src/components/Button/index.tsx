8496bbb8f5ef30ea9623ff8f65d4955e3637d593
Gui-dev <adrianon10@gmail.com>
2022-12-14 04:11:54 -0300

    feat: add property loading

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,22 @@
-import { TouchableOpacityProps } from 'react-native'
+import { ActivityIndicator, TouchableOpacityProps } from 'react-native'
 
 import { Text } from '../Text'
 import { Container } from './style'
 
 type ButtonProps = TouchableOpacityProps & {
   label: string
+  loading?: boolean
 }
 
-export const Button = ({ label, ...rest }: ButtonProps) => {
+export const Button = ({ label, loading, ...rest }: ButtonProps) => {
   return (
     <Container {...rest}>
-      <Text color="#FFF" weight="600">{label}</Text>
+      {!loading && (
+        <Text color="#FFF" weight="600">{label}</Text>
+      )}
+      {loading && (
+        <ActivityIndicator color="#FFF" size="small" />
+      )}
     </Container>
   )
 }
